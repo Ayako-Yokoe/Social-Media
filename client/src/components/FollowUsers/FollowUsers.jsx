6622339a2c0d41b907a228a78f42152d448e81ef
Ayako-Yokoe <niluvsuall@gmail.com>
2022-09-19 20:47:26 -0700

    Add useCallback still working

--- client/src/components/FollowUsers/FollowUsers.jsx
+++ client/src/components/FollowUsers/FollowUsers.jsx
@@ -1,29 +1,56 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext, useState, useEffect, useCallback } from "react"
 import axios from "axios"
 import Context from "../../context"
 
-const FollowUsers = ({ person, loadFollowers }) => {
+const FollowUsers = ({ person, loadFollowers, loadAllUsers }) => {
   const { setIsLoading, user } = useContext(Context)
   const [currentUser, setCurrentUser] = useState(null)
 
-  const loadUserInfo = () => {
+  console.log("currentUser child ", currentUser)
+
+  let loadUserInfo = null
+
+  loadUserInfo = useCallback(async () => {
     setIsLoading(true)
     try {
-      //
-      axios.get(`http://localhost:3001/user/${user.id}`).then((res) => {
-        setCurrentUser(res.data)
-      })
+      const response = axios.get(`http://localhost:3001/user/${user.id}`)
+
+      console.log("child reponse ", response)
+
+      if (response && response.data) {
+        setCurrentUser(response)
+      } else {
+        setCurrentUser(person)
+      }
+      //setCurrentUser(response)
     } catch (error) {
       console.log(error)
     }
     setIsLoading(false)
-  }
+  })
 
-  // userCallback
   useEffect(() => {
     loadUserInfo()
-    //}, [setIsLoading, user, currentUser, setCurrentUser])
-  }, [])
+  }, [setIsLoading, currentUser])
+
+  // const loadUserInfo = () => {
+  //   setIsLoading(true)
+  //   try {
+  //     //
+  //     axios.get(`http://localhost:3001/user/${user.id}`).then((res) => {
+  //       setCurrentUser(res.data)
+  //     })
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  //   setIsLoading(false)
+  // }
+
+  // // useCallback
+  // useEffect(() => {
+  //   loadUserInfo()
+  //   //}, [setIsLoading, user, currentUser, setCurrentUser])
+  // }, [])
 
   const removeFollow = async () => {
     return await axios.post("http://localhost:3001/followers/delete", {
@@ -38,12 +65,15 @@ const FollowUsers = ({ person, loadFollowers }) => {
     })
   }
   const updateNumberOfFollowers = async (numberOfFollowers) => {
+    console.log("numberOfFollowers ", numberOfFollowers)
+
     return await axios.post("http://localhost:3001/user/followers", {
       id: person.id,
       numberOfFollowers,
     })
   }
   const updateNumberOfFollowing = async (numberOfFollowing) => {
+    console.log("numberOfFollowing ", numberOfFollowing)
     return await axios.post("http://localhost:3001/user/following", {
       id: user.id,
       numberOfFollowing,
@@ -51,34 +81,40 @@ const FollowUsers = ({ person, loadFollowers }) => {
   }
 
   const toggleFollowers = async () => {
-    await loadFollowers(person.id)
+    //await loadFollowers(person.id)
+    await loadUserInfo()
 
     try {
       if (person.hasFollowed) {
+        console.log("remove")
         await removeFollow()
         await updateNumberOfFollowers(
           person.number_of_followers ? person.number_of_followers - 1 : 0
           //(person.number_of_followers = 0)
         )
         await updateNumberOfFollowing(
-          currentUser.number_of_following
-            ? currentUser.number_of_following - 1
+          currentUser?.number_of_following
+            ? currentUser?.number_of_following - 1
             : 0
           //(user.number_of_following = 0)
         )
       } else {
+        console.log("follow")
         await follow()
         await updateNumberOfFollowers(
           person.number_of_followers ? person.number_of_followers + 1 : 1
           //(person.number_of_followers = 0)
         )
         await updateNumberOfFollowing(
-          currentUser.number_of_following
-            ? currentUser.number_of_following + 1
+          currentUser?.number_of_following
+            ? currentUser?.number_of_following + 1
             : 1
           //(user.number_of_following = 0)
         )
       }
+      //
+      await loadFollowers(person.id)
+      //
     } catch (error) {
       console.log(error)
     }
@@ -96,6 +132,7 @@ const FollowUsers = ({ person, loadFollowers }) => {
 
         <button className="button fc-button" onClick={toggleFollowers}>
           {person.hasFollowed ? "Unfollow" : "Follow"}
+          {/* {currentUser?.hasFollowed ? "Unfollow" : "Follow"} */}
         </button>
       </div>
     </div>
--- client/src/components/FollowersCard/FollowersCard.jsx
+++ client/src/components/FollowersCard/FollowersCard.jsx
@@ -8,16 +8,49 @@ import FollowUsers from "../FollowUsers/FollowUsers"
 const FollowersCard = () => {
   const { setIsLoading, user } = useContext(Context)
   const [usersYouMayKnow, setUsersYouMayKnow] = useState([])
+  //const [updatedFollowerInfo, setUpdatedFollowerInfo] = useState([])
 
-  const loadAllUsers = async () => {
-    setIsLoading(true)
-    axios.get("http://localhost:3001/user").then((res) => {
-      setUsersYouMayKnow(res.data)
-    })
-    setIsLoading(false)
+  console.log("usersYouMayKnow parent ", usersYouMayKnow)
+
+  let loadAllUsers = null
+  let loadFollowers = null
+
+  useEffect(() => {
+    loadAllUsers()
+  }, [loadAllUsers])
+
+  useEffect(() => {
+    loadFollowers()
+  }, [loadFollowers, usersYouMayKnow, setUsersYouMayKnow])
+
+  // loadAllUsers = useCallback(async () => {
+  //   try {
+  //     setIsLoading(true)
+  //     const response = await axios.get("http://localhost:3001/user", {
+  //       params: { _limit: 5 },
+  //     })
+  //     setUsersYouMayKnow(response.data)
+  //     setIsLoading(false)
+  //     await loadFollowers()
+  //   } catch (err) {
+  //     setIsLoading(false)
+  //   }
+  // })
+
+  loadAllUsers = async () => {
+    try {
+      setIsLoading(true)
+      const response = await axios.get("http://localhost:3001/user", {
+        params: { _limit: 5 },
+      })
+      setUsersYouMayKnow(response.data)
+      setIsLoading(false)
+    } catch (err) {
+      setIsLoading(false)
+    }
   }
 
-  const loadFollowers = async (id) => {
+  loadFollowers = useCallback(async (id) => {
     if (!id) return
 
     if (user.id && id) {
@@ -28,14 +61,19 @@ const FollowersCard = () => {
           person_id: id,
         })
 
+        console.log("parent response ", response)
+
         let updatedUsersYouMayKnow = usersYouMayKnow.map((person) =>
           person.id === id
             ? {
                 ...person,
-                hasFollowed: response && response.data.person_id ? false : true,
+                //hasFollowed: response && response.data.person_id ? false : true,
+                hasFollowed: response && response.data.person_id ? true : false,
               }
             : person
         )
+
+        console.log("updatedUsersYouMayKnow ", updatedUsersYouMayKnow)
 
         setUsersYouMayKnow(updatedUsersYouMayKnow)
         setIsLoading(false)
@@ -44,17 +82,81 @@ const FollowersCard = () => {
         setIsLoading(false)
       }
     }
-  }
+  })
 
-  useEffect(() => {
-    loadAllUsers()
-    loadFollowers()
-  }, [setIsLoading, setUsersYouMayKnow])
+  // loadFollowers = useCallback(async (id) => {
+  //   if (!id) return
+
+  //   if (user.id && id) {
+  //     try {
+  //       setIsLoading(true)
+  //       const response = await axios.post("http://localhost:3001/followers", {
+  //         follower_id: user.id,
+  //         person_id: id,
+  //       })
+  //       let updatedUsersYouMayKnow = usersYouMayKnow.map((person) =>
+  //         person.id === id
+  //           ? {
+  //               ...person,
+  //               hasFollowed: response && response.data.person_id ? false : true,
+  //             }
+  //           : person
+  //       )
+  //       setUsersYouMayKnow(updatedUsersYouMayKnow)
+  //       setIsLoading(false)
+  //     } catch (error) {
+  //       console.log(error)
+  //       setIsLoading(false)
+  //     }
+  //   }
+  // })
+
+  // const loadAllUsers = async () => {
+  //   setIsLoading(true)
+  //   axios.get("http://localhost:3001/user").then((res) => {
+  //     setUsersYouMayKnow(res.data)
+  //   })
+  //   setIsLoading(false)
+  // }
+
+  // const loadFollowers = async (id) => {
+  //   if (!id) return
+
+  //   if (user.id && id) {
+  //     try {
+  //       setIsLoading(true)
+  //       const response = await axios.post("http://localhost:3001/followers", {
+  //         follower_id: user.id,
+  //         person_id: id,
+  //       })
+
+  //       let updatedUsersYouMayKnow = usersYouMayKnow.map((person) =>
+  //         person.id === id
+  //           ? {
+  //               ...person,
+  //               hasFollowed: response && response.data.person_id ? false : true,
+  //             }
+  //           : person
+  //       )
+
+  //       setUsersYouMayKnow(updatedUsersYouMayKnow)
+  //       setIsLoading(false)
+  //     } catch (error) {
+  //       console.log(error)
+  //       setIsLoading(false)
+  //     }
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   loadAllUsers()
+  //   loadFollowers()
+  // }, [setIsLoading, setUsersYouMayKnow])
 
   return (
     <div className="followersCard">
       <h3>People You May Know</h3>
-      {usersYouMayKnow.map((person) => {
+      {usersYouMayKnow?.map((person) => {
         if (person.id !== user.id) {
           return (
             <FollowUsers
